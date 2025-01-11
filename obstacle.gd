07a477289d8700091a6318b51e86af090a4e673f
LU347 <103535661+LU347@users.noreply.github.com>
2025-01-10 23:11:55 -0600

    Fixed velocity issue + updated spawner cooldown

--- obstacle.gd
+++ obstacle.gd
@@ -1,15 +1,14 @@
 extends RigidBody2D
 
-@export var VELOCITY: float = -100
+@export var VELOCITY: float = -300
 
 #TODO:
-# fix velocity since it's not static.
-
+# fix velocity since it's not
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     linear_velocity = Vector2(VELOCITY, 0)  # Set the velocity in the x-axis
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    pass
+    linear_velocity = Vector2(VELOCITY,0)
 
