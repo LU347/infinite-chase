df8abea624a1c9e32a9d08d7f21a9827afb67f60
LU347 <103535661+LU347@users.noreply.github.com>
2025-01-19 00:20:27 -0600

    Update title_screen.gd
    Scales the game to the user's screen resolution

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -2,6 +2,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var screen_resolution = DisplayServer.window_get_size_with_decorations()
+	DisplayServer.window_set_size(screen_resolution)
+	DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_MAXIMIZED)
+	
 	$SoundManager.play_sound("title")
 
 func _on_button_pressed() -> void:
