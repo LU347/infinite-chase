6f9854dea72d374aaad9a9fdfb31984fd2f89d29
LU347 <103535661+LU347@users.noreply.github.com>
2025-01-30 22:51:46 -0600

    Fixed platform moving too early

--- Scripts/platform.gd
+++ Scripts/platform.gd
@@ -7,19 +7,20 @@ extends Node2D
 var can_move: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if can_move:
 		for child in get_children():
-			if child is not Sprite2D:
-				continue
-			_move_sprite(child, delta)
+			if child is Sprite2D:
+				_move_sprite(child)
 
-func _move_sprite(sprite: Sprite2D, _delta: float) -> void:
-	#var texture_width = sprite.texture.get_size().x * sprite.scale.x
+func _move_sprite(sprite: Sprite2D) -> void:
 	var platform = sprite.get_node("PlatformTip")
 	sprite.position.x += VELOCITY
 
-	if platform.global_position.x <= $EndPosition.global_position.x:
+	var platform_local_position = to_local(platform.global_position).x
+	var end_position_local = to_local($EndPosition.global_position).x
+
+	if platform_local_position <= end_position_local:
 		sprite.position.x = $SpawnPosition.position.x
 
 func _on_main_start_game_objects() -> void:
